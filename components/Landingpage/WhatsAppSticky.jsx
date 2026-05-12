72be89d2899f9de55e5a2ed88a24366640fe1e96
Anshuman <anshutyagi799@gmail.com>
2026-05-12 11:07:13 +0530

    button alignment fix

--- components/Landingpage/WhatsAppSticky.jsx
+++ components/Landingpage/WhatsAppSticky.jsx
@@ -5,7 +5,7 @@ const WhatsAppSticky = () => {
   return (<>
     <a href="https://wa.me/919990730939"
       target="_blank"
-      className="bg-green-500 hover:bg-green-600 p-2 rounded-full flex items-center justify-center transition fixed bottom-4 right-4  lg:bottom-20 lg:right-10 z-50 animate-bounce"
+      className="bg-green-500 hover:bg-green-600 p-2 rounded-full flex items-center justify-center transition fixed bottom-18 right-4  lg:bottom-20 lg:right-10 z-50 animate-bounce"
     >
       <FaWhatsapp size={35} className="text-white" />
     </a>
