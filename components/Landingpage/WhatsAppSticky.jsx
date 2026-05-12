61f10c056da1a7877954f3c8708c9de604412406
Anshuman <anshutyagi799@gmail.com>
2026-05-12 11:16:56 +0530

    final fix

--- components/Landingpage/WhatsAppSticky.jsx
+++ components/Landingpage/WhatsAppSticky.jsx
@@ -1,20 +1,22 @@
 "use client";
-import { FaWhatsapp } from "react-icons/fa";
+import { FaPhoneAlt, FaWhatsapp } from "react-icons/fa";
 
 const WhatsAppSticky = () => {
   return (<>
     <a href="https://wa.me/919990730939"
       target="_blank"
-      className="bg-green-500 hover:bg-green-600 p-2 rounded-full flex items-center justify-center transition fixed bottom-18 right-4  lg:bottom-20 lg:right-10 z-50 animate-bounce"
+      rel="noopener noreferrer"
+      aria-label="Chat on WhatsApp "
+      className="fixed bottom-5 right-4 z-50 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition animate-bounce"
     >
-      <FaWhatsapp size={35} className="text-white" />
+      <FaWhatsapp size={30} />
     </a>
 
     <a href="tel:9990730939"
-      target="_blank"
-      className="bg-red-500 hover:bg-red-600 p-2 rounded-full flex items-center justify-center transition fixed bottom-4 right-4  lg:bottom-5 lg:right-10 z-50 animate-bounce"
+      aria-label="Call Us"
+      className="fixed bottom-21 right-4 z-50 bg-red-500 text-white p-3 rounded-full shadow-lg hover:bg-red-600 transition animate-bounce"
     >
-      <FaWhatsapp size={35} className="text-white" />
+      <FaPhoneAlt size={27} />
     </a>
   </>);
 };
